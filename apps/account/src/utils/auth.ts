ebdab1075280446249a56ba69989d68fec9b6f28
Andrew Miracle <me@andrewmiracle.com>
2021-03-01 15:16:47 +0000

    initialize routes and utils

---
+++ apps/account/src/utils/auth.ts
@@ -0,0 +1,8 @@
+/**
+ * Authentication handler actions for the client application
+ * Methods will include
+ * @method logout - clear cookies after call to api
+ * @method setToken - persist cookies to browser
+ * @method getToken - get Token from browser
+ * @method isAuth - Check that user is authenticated
+ */
---
+++ apps/account/src/utils/http.ts
@@ -0,0 +1,5 @@
+/**
+ * Keep the HTTP Adapter here. some facade pattern would be ideal for (GET, POST)
+ * @method list - call an API get endpoint with Query parameters
+ * @method post - call and API post endpoint with a req body
+ */
