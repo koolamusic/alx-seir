7899a6182280bf28e287a7cdc9acc010f220f785
Andrew Miracle <me@andrewmiracle.com>
2021-03-02 00:00:07 +0000

    enable authentication middleware

--- api/src/controllers/messenger/routes.ts
+++ api/src/controllers/messenger/routes.ts
@@ -6,14 +6,10 @@ const router = express.Router();
 
 
 /* Proxy to the Jokes API */
-router.get('/jokes/:id',
-    //  [strategy.isAuthenticated], 
-    controller.jokesApiProxyMiddleware);
+router.get('/jokes/:id', [strategy.isAuthenticated], controller.jokesApiProxyMiddleware);
 
 /* Proxy to the KITSU Manga API */
-router.get('/manga',
-    // [strategy.isAuthenticated], 
-    controller.mangaApiProxyMiddleware);
+router.get('/manga', [strategy.isAuthenticated], controller.mangaApiProxyMiddleware);
 router.get('/manga/:id', [strategy.isAuthenticated], controller.mangaApiProxyMiddleware);
 
 export default router;
