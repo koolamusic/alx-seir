b4dbb312491c3f1ff2267314c71ba4944cd41574
Andrew Miracle <me@andrewmiracle.com>
2021-03-10 20:38:51 +0000

    trigger signout from server

--- apps/account/src/pages/logout.tsx
+++ apps/account/src/pages/logout.tsx
@@ -1,5 +1,25 @@
 import { NextPageContext } from 'next';
 import * as Auth from '../utils/auth'
+import ResourceFactory from '../utils/adapter'
+import { AxiosRequestConfig } from 'axios'
+
+
+const baseURL = process.env.NEXT_PUBLIC_API_URL;
+
+
+const defaultConfig: AxiosRequestConfig = {
+    baseURL: baseURL,
+    withCredentials: true,
+    headers: {
+        'X-Request-With': 'XMLHttpRequest',
+    }
+};
+
+ResourceFactory.updateDefaults(defaultConfig)
+class SignOut extends ResourceFactory.createResource("/v1/auth/logout") { }
+
+
+
 
 export default function Logout() {
     return (
@@ -14,7 +34,10 @@ export default function Logout() {
 
 
 export async function getServerSideProps(ctx: NextPageContext) {
-    Auth.logoutUser(ctx, '/');
+    await SignOut.get().then(() => {
+        Auth.logoutUser(ctx, '/');
+    })
+
     return {
         props: null
     }
